75efdcdd54fbc71eb77e578f0c26b0ae762c71fc
huangjiayu <hjyiwxz@gmail.com>
2025-05-26 21:39:11 +0800

    doc: 优化文档

--- user-admin.sql
+++ user-admin.sql
@@ -110,41 +110,6 @@ CREATE TABLE sys_user_data_scope (
     PRIMARY KEY (user_id, dept_id)
 ) ENGINE=InnoDB COMMENT='用户数据权限表';
 
--- 操作日志表
-CREATE TABLE sys_operation_log (
-    oper_id         BIGINT      NOT NULL AUTO_INCREMENT COMMENT '日志主键',
-    title           VARCHAR(50)          DEFAULT '' COMMENT '模块标题',
-    business_type   INT                  DEFAULT 0 COMMENT '业务类型（0其它 1新增 2修改 3删除）',
-    method          VARCHAR(100)         DEFAULT '' COMMENT '方法名称',
-    request_method  VARCHAR(10)          DEFAULT '' COMMENT '请求方式',
-    operator_type   INT                  DEFAULT 0 COMMENT '操作类别（0其它 1后台用户 2手机端用户）',
-    oper_name       VARCHAR(50)          DEFAULT '' COMMENT '操作人员',
-    dept_name       VARCHAR(50)          DEFAULT '' COMMENT '部门名称',
-    oper_url        VARCHAR(255)         DEFAULT '' COMMENT '请求URL',
-    oper_ip         VARCHAR(128)         DEFAULT '' COMMENT '主机地址',
-    oper_location   VARCHAR(255)         DEFAULT '' COMMENT '操作地点',
-    oper_param      VARCHAR(2000)        DEFAULT '' COMMENT '请求参数',
-    json_result     VARCHAR(2000)        DEFAULT '' COMMENT '返回参数',
-    status          INT                  DEFAULT 0 COMMENT '操作状态（0正常 1异常）',
-    error_msg       VARCHAR(2000)        DEFAULT '' COMMENT '错误消息',
-    oper_time       DATETIME             DEFAULT CURRENT_TIMESTAMP COMMENT '操作时间',
-    PRIMARY KEY (oper_id)
-) ENGINE=InnoDB AUTO_INCREMENT=100 COMMENT='操作日志记录';
-
--- 登录日志表
-CREATE TABLE sys_login_log (
-    info_id         BIGINT      NOT NULL AUTO_INCREMENT COMMENT '访问ID',
-    user_name       VARCHAR(50)          DEFAULT '' COMMENT '用户账号',
-    ipaddr          VARCHAR(128)         DEFAULT '' COMMENT '登录IP地址',
-    login_location  VARCHAR(255)         DEFAULT '' COMMENT '登录地点',
-    browser         VARCHAR(50)          DEFAULT '' COMMENT '浏览器类型',
-    os              VARCHAR(50)          DEFAULT '' COMMENT '操作系统',
-    status          CHAR(1)              DEFAULT '0' COMMENT '登录状态（0成功 1失败）',
-    msg             VARCHAR(255)         DEFAULT '' COMMENT '提示消息',
-    login_time      DATETIME             DEFAULT CURRENT_TIMESTAMP COMMENT '访问时间',
-    PRIMARY KEY (info_id)
-) ENGINE=InnoDB AUTO_INCREMENT=100 COMMENT='系统访问记录';
-
 -- 重置密码令牌表
 CREATE TABLE sys_password_reset (
     reset_id        BIGINT      NOT NULL AUTO_INCREMENT COMMENT '重置ID',
